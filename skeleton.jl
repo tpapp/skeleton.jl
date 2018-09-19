daa601249eadc4036a6cebdaa4adf18430fb2f72
Tamas K. Papp <tkpapp@gmail.com>
2018-09-19 11:01:08 +0200

    Use Project.toml from template.

--- skeleton.jl
+++ skeleton.jl
@@ -1,74 +1,79 @@
 #!/usr/bin/env julia
 
-using Pkg
+using Pkg, UUIDs
+
+# sanity checks and parsing input arguments
 
 minver = v"0.7"
-
 err(msg) = (printstyled(stderr, msg * '\n'; bold = true, color = :blue); exit(1))
-
 VERSION ≥ minver || err("need at least version $(minver)")
-
 1 ≤ length(ARGS) ≤ 2 || err("Usage: [julia] skeleton.jl destination [template]")
-
 destdir = ARGS[1]
-
 isfile(destdir) && err("destination $(destdir) is a file")
-
 template = length(ARGS) == 2 ? ARGS[2] : "template"
-
 pkgname = basename(destdir)
 
 if isempty(pkgname)
-    pkgname = basename(dirname(destdir)) # trailing /
+    pkgname = basename(dirname(destdir)) # add trailing /
 end
 
+
+# utility functions
+
+"Get an option from `git`."
 function getgitopt(opt)
     try
         chomp(read(`git config --get $(opt)`, String))
     catch
-        error(ArgumentError("couldn't get option $(opt)"))
+        err("couldn't get option $(opt)")
     end
 end
 
-ghuser = getgitopt("github.user")
-
-@info "package name is \"$(pkgname)\", using template \"$(template)\", github user \"$(ghuser)\""
-
-if isdir(destdir)
-    @info "destination directory exists, skipping package generation"
-else
-    @info "destination directory does not exist, calling `Pkg.generate`"
-    Pkg.API.generate(destdir)
-end
-
-const thisdir = @__DIR__
-
-srcroot = joinpath(thisdir, template)
-
-function replace_multiple(str, pairs)
-    for pair in pairs
+"Replace multiple pairs in `str`, using `replace` iteratively."
+function replace_multiple(str, replacements)
+    for pair in replacements
         str = replace(str, pair)
     end
     str
 end
 
-for (root, dirs, files) in walkdir(srcroot)
-    relroot = relpath(root, srcroot)
-    mkpath(normpath(destdir, relroot))
-    for file in files
-        srcfile = joinpath(root, file)
-        destfile = normpath(joinpath(destdir, relroot, file))
-        if isfile(destfile)
-            println("$(destfile) exists, skipping")
-        else
-            println("$(srcfile) -> $(destfile)")
-            srcstring = read(srcfile, String)
-            deststring = replace_multiple(srcstring, ["{PKGNAME}" => pkgname,
-                                                      "{GHUSER}" => ghuser])
-            write(destfile, deststring)
+function copy_and_substitute(srcroot, destdir, replacements)
+    for (root, dirs, files) in walkdir(srcroot)
+        relroot = relpath(root, srcroot)
+        mkpath(normpath(destdir, relroot))
+        for file in files
+            srcfile = joinpath(root, file)
+            destfile = normpath(joinpath(destdir, relroot, file))
+            if isfile(destfile)
+                println("$(destfile) exists, skipping")
+            else
+                println("$(srcfile) -> $(destfile)")
+                srcstring = read(srcfile, String)
+                deststring = replace_multiple(srcstring, )
+                write(destfile, deststring)
+            end
         end
     end
 end
+
+
+# replacements
+
+replacements = ["{UUID}" => uuid
+                "{PKGNAME}" => pkgname,
+                "{GHUSER}" => getgitopt("github.user"),
+                "{USERNAME}" => getgitopt("user.name"),
+                "{USEREMAIL}" => getgitopt("user.email")]
+
+if isdir(destdir)
+    @info "destination directory exists, skipping package generation"
+    exit(0)
+end
+
+@info "replacements are" replacements
+
+srcroot = joinpath(@__DIR__, template)
+copy_and_substitute(srcroot, destdir, replacements)
 
 @info "calling git init"
 run(`git init $destdir`)
