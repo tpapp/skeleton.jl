62478407e27e9c8e3776062ed5cf377c161d7e8d
Tamas K. Papp <tkpapp@gmail.com>
2018-11-09 13:28:21 +0100

    use Documenter v0.20 (#17)
    Remove arguments for which we use the default. Also specify checkdocs = :exports.

--- template/docs/make.jl
+++ template/docs/make.jl
@@ -3,14 +3,11 @@ using Documenter, {PKGNAME}
 makedocs(
     modules = [{PKGNAME}],
     format = :html,
+    checkdocs = :exports,
     sitename = "{PKGNAME}.jl",
     pages = Any["index.md"]
 )
 
 deploydocs(
     repo = "github.com/{GHUSER}/{PKGNAME}.jl.git",
-    target = "build",
-    julia = "1.0",
-    deps = nothing,
-    make = nothing,
 )
